62485e5e13f8ded5db702b49bb2f1470561f76ac
Oğuzhan Bütüner <oguzhanraifbutuner@gmail.com>
2024-12-05 04:00:52 +0300

    home page update

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import homeBanner from "../../Assets/Iteration-1-assets/home-banner.png";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Tabs, Tab, Box } from "@mui/material";
 import logo from "../../Assets/Iteration-1-assets/logo.svg";
 import { useHistory } from "react-router-dom";
-
+import "./IntroPage.css";
 
 function IntroPage() {
   const history = useHistory();
@@ -12,26 +11,76 @@ function IntroPage() {
     history.push("/siparis-formu");
   };
 
-
-
   return (
-    <div className="intro-page">
-      <img src={logo} alt="Logo" className="logo" />
-      <img src={homeBanner} alt="" className="intro-page-home-banner" />
-      <div className="intro-page-heading-container">
-        <Typography variant="h1" className="intro-page-heading">
-          KOD ACIKTIRIR, <br /> PİZZA DOYURUR!
-        </Typography>
-        <Button
-          variant="contained"
-          color="secondary"
-          className="intro-page-button"
-          onClick={handleClick}
-        >
-          <Typography variant="h6">Acıktım</Typography>
-        </Button>
+    <>
+      <div className="intro-page-banner-container">
+        <div className="intro-page-hero">
+          <img className="app-bar-logo" src={logo} alt="Logo" />
+          <div className="intro-page-hero-text-container">
+            <h2 className="intro-page-hero-styled-text">fırsatı kaçırma</h2>
+            <h1 className="intro-page-hero-heading">
+              KOD ACIKTIRIR, <br /> PİZZA DOYURUR!
+            </h1>
+            <Button
+              variant="contained"
+              color="secondary"
+              className="intro-page-hero-button"
+              onClick={handleClick}
+            >
+              <Typography variant="h6">Acıktım</Typography>
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="intro-page-content-wrapper">
+        <div className="intro-page-menu-bar">
+          <div className="intro-page-menu-item">
+            <img src="../../Assets/Iteration-2-assets/icons/1.svg" alt="" />
+            <span>YENİ! Kore</span>
+          </div>
+          <div className="intro-page-menu-item">
+            <img src="../../Assets/Iteration-2-assets/icons/2.svg" alt="" />
+            <span>Pizza</span>
+          </div>
+          <div className="intro-page-menu-item">
+            <img src="../../Assets/Iteration-2-assets/icons/3.svg" alt="" />
+            <span>Burger</span>
+          </div>
+          <div className="intro-page-menu-item">
+            <img src="../../Assets/Iteration-2-assets/icons/4.svg" alt="" />
+            <span>Kızartmalar</span>
+          </div>
+          <div className="intro-page-menu-item">
+            <img src="../../Assets/Iteration-2-assets/icons/5.svg" alt="" />
+            <span>Fast Food</span>
+          </div>
+          <div className="intro-page-menu-item">
+            <img src="../../Assets/Iteration-2-assets/icons/6.svg" alt="" />
+            <span>Gazlı İçecek</span>
+          </div>
+        </div>
+        <div className="intro-page-cta-wrapper">
+          <div className="intro-page-cta-left-column">
+            <h2 className="cta-left-heading">
+              Özel <br /> Lezzetus
+            </h2>
+            <p className="cta-left-subtitle">Position: Absolute Acı Burger</p>
+            <Button
+              variant="contained"
+              color="secondary"
+              className="cta-left-button"
+              onClick={handleClick}
+            >
+              <span>Sipariş Ver</span>
+            </Button>
+          </div>
+          <div className="intro-page-cta-right-column">
+            <div className="cta-righ-card-1">asdasdas</div>
+            <div className="cta-righ-card-2"></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
